73bea7bc1eb123e19ee485f2658fb65fc7071d25
emilypandinha <136619754+emilypandinha@users.noreply.github.com>
2024-08-06 12:03:25 -0300

    ok

--- script.js
+++ script.js
@@ -8,15 +8,16 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "PERGUNTA 1",
+        enunciado: "Você acha que o homem tem ralação direta com a existência atual de enchentes e inundações",
         alternativas: [
             {
-                texto: "RESPOSTA 1!",
-                afirmacao: "AFIRMATIVA DA RESPOSTA 1r. "
+                texto: "Sim, acredito que tenha relação",
+                afirmacao: " As enchentes são fenomenos naturais, mas a ação humana intensifica esse problema. Os rios raissa têm leitos maiores e menores, e em áreas planas(planícies de inundações), o leito maior é inundado durante cheias  "  
+       ,
             },
             {
-                texto: "RESPOSTA 2",
-                afirmacao: "AFIRMATIVA RESPOSTA 2."
+                texto: "Não, acredito que não tenha relação ",
+                afirmacao: " As enchentes também ocorrem naturalmente, independentemente da ação humana. As elaborações dos cursos d'ag."
             }
         ]
     },
