9ab78920f81bb46e6b7056b526946bf9b2a9980f
emilypandinha <136619754+emilypandinha@users.noreply.github.com>
2024-08-13 11:45:14 -0300

    ok

--- script.js
+++ script.js
@@ -49,3 +49,21 @@ const perguntas = [
     },
       
 ];
+
+
+let atual=0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+      const botaoAlternativas = document.createElement("button");
+      botaoAlternativas.textContent = alternativa.texto;
+      caixaAlternativas.appendChild(botaoAlternativas);
+    }
+  }
+mostraPergunta();
